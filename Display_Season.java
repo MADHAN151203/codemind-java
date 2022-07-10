948b25abe931aa11f81ea6e2185f4dfdd1a43a6f
MADHAN151203 <96331328+MADHAN151203@users.noreply.github.com>
2022-07-10 15:25:11 +0530

    import java.util.Scanner;  class season {     public static void main(String[]args)     {         Scanner sc=new Scanner(System.in);         int n;         n=sc.nextInt();         switch(n)         {             case 4:             case 5:             case 6:                 System.out.println("Summer");                 break;             case 7:             case 8:             case 9:             case 10:             case 11:                 System.out.println("Rainy");                 break;                 case 12:                 case 1:                     System.out.println("Winter");                     break;                 case 2:                 case 3:                     System.out.println("Spring");                     break;                     default:                     System.out.println("-1");                     break;
            }
            
        }
        
    }

---
+++ Display_Season.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+ class season
+{
+    public static void main(String[]args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n;
+        n=sc.nextInt();
+        switch(n)
+        {
+            case 4:
+            case 5:
+            case 6:
+                System.out.println("Summer");
+                break;
+            case 7:
+            case 8:
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Rainy");
+                break;
+                case 12:
+                case 1:
+                    System.out.println("Winter");
+                    break;
+                case 2:
+                case 3:
+                    System.out.println("Spring");
+                    break;
+                    default:
+                    System.out.println("-1");
+                    break;
+                    
+        }
+        
+    }
+    
+}
